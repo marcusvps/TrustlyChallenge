c11d7435465c7c733b0017e6ec93a96e05f53329
marcus.v.pereira <marcus.v.pereira@pbh.gov.br>
2021-06-03 11:32:39 -0300

    Creating return DTO with file information. Renaming the endpoint to retrieve information about files.

--- src/main/java/com/trustly/challenge/controller/WebScrapingController.java
+++ src/main/java/com/trustly/challenge/controller/WebScrapingController.java
@@ -1,5 +1,6 @@
 package com.trustly.challenge.controller;
 
+import com.trustly.challenge.dto.FileDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,14 +8,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
-@RequestMapping("/ws")
+@RequestMapping("/files")
 public class WebScrapingController {
 
-    @GetMapping(path = "/test")
+    @GetMapping
     @ResponseBody
-    public ResponseEntity<String> test(){
-        return new ResponseEntity<>("Is online", HttpStatus.OK);
+    public ResponseEntity<List<FileDTO>> getFilesInformation(){
+        FileDTO fileDTO = new FileDTO();
+        fileDTO.setCount(1);
+        fileDTO.setExtension(".java");
+        fileDTO.setLines(145);
+        fileDTO.setBytes(493349L);
+
+        FileDTO fileDTO2 = new FileDTO();
+        fileDTO2.setCount(1);
+        fileDTO2.setExtension(".java");
+        fileDTO2.setLines(145);
+        fileDTO2.setBytes(493349L);
+
+        return new ResponseEntity<>(Arrays.asList(fileDTO, fileDTO2), HttpStatus.OK);
+
 
     }
 }
---
+++ src/main/java/com/trustly/challenge/dto/FileDTO.java
@@ -0,0 +1,17 @@
+package com.trustly.challenge.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class FileDTO {
+    private String extension;
+    private Integer count;
+    private Integer lines;
+    private Long bytes;
+
+
+}
